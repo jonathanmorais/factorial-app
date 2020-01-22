f878056dbca6fd5b85f9dc52c983a5d7d259a99e
jonathanmorais <jonathan.m.lucena@gmail.com>
2020-01-22 17:23:41 -0300

    err tratement

--- src/main.go
+++ src/main.go
@@ -34,16 +34,16 @@ func getFactorial(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r) //pega variaveis "path parameter"
 	w.WriteHeader(http.StatusOK)
-	number, ok := vars["num"]
-	if !ok {
-		return
-	} else if number == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode("Error")
+	number, _ := vars["num"]
+	_, err := strconv.ParseInt(number, 10, 64)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+	} else if err == nil {
+		w.Write([]byte(fmt.Sprint("Is Empty")))
 	}
 	n, _ := strconv.ParseInt(number, 10, 64)
 	calcFactorial := (factorial(big.NewInt(n)))
-	w.Write([]byte(fmt.Sprintf("%+v", calcFactorial)))
+	w.Write([]byte(fmt.Sprintf("%v", calcFactorial)))
 }
 
 func factorial(x *big.Int) *big.Int {
